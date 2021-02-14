f76c8ae9b825d391cf248ad992dd928e14e59914
SushantBait <53819730+SushantBait@users.noreply.github.com>
2021-02-14 12:36:28 +0530

    Update Explicit.java

--- 13Feb/src/com/student/Explicit.java
+++ 13Feb/src/com/student/Explicit.java
@@ -1,18 +1,14 @@
 /* 
  * @Author:Sushant
- * 
  */
 package com.student;
-
 public class Explicit {
-
 	public static void main(String[] args) {
-		double d = 100.04;  
+	double d = 100.104;  
         long l = (long)d; //explicit type casting  
         int i = (int)l;  
         System.out.println("Double value "+d); 
-        System.out.println("Long value "+l);  //fractional part lost here
+        System.out.printsln("Long value "+l);  //fractional part lost here
         System.out.println("Int value "+i);  //fractional part lost here
 	}
-
 }
