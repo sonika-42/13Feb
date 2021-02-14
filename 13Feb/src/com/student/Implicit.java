cf03724710be67e80a1a00c7fbac1bc3e2c68efd
SushantBait <53819730+SushantBait@users.noreply.github.com>
2021-02-14 12:34:57 +0530

    Update Implicit.java

--- 13Feb/src/com/student/Implicit.java
+++ 13Feb/src/com/student/Implicit.java
@@ -1,18 +1,14 @@
 package com.student;
-
 public class Implicit {
-
-	public static void main(String[] args) {
-		
-		int i = 123;  //Implicit conversion 
+	public static void main(String[] args) {		
+	int i = 123;  //Implicit conversion 
         long l = i;     
         float f = l;
-        Integer a=i;
+        Integer a = i;
       
         System.out.println("Int value "+i); 
         System.out.println("Long value "+l); 
         System.out.println("Float value "+f);
         System.out.println("Integer (Convert in Wrapper)= "+a);
 	}
-
 }
